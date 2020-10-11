b4502abf86eed93b18457d24aca28ff79f6b95b5
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2020-10-11 17:16:08 +0530

    Added simple poll

---
+++ commands/misc/simplepoll.js
@@ -0,0 +1,40 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    var a = message.id;
+    message.channel.messages.fetch(a).then(msg => msg.delete({ timeout: 100 }));
+    const emotes = ['🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯'];
+    var neb = args.join(' ');
+    var text = "";
+    var words = neb.split('"');
+    var finalwords = [];
+    for(var i = 0;i<words.length;i++){
+        if(i%2!=0){
+            finalwords.push(words[i]);
+        }
+    }
+    if(finalwords.length>11) return message.reply("Woah chill! Keep it less than 10 options");
+    for(var i=1;i<finalwords.length;i++){
+        text+=emotes[i-1]+" "+finalwords[i]+"\n";
+    }
+    var embed = new discord.MessageEmbed()
+        .setTitle(finalwords[0])
+        .setDescription(text)
+        .addField("Poll by: ", message.author);
+    message.channel.send(embed).then(msg =>{
+        for(var i=1;i<finalwords.length;i++){
+            try{
+                msg.react(emotes[i-1]);
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+
+    });
+};
+
+module.exports.help = {
+	name: "poll",
+    aliases: []
+};
