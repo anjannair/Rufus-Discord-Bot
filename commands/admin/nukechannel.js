c248281ea07c7e4437638d142d0ba76c6a788319
Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
2021-03-31 15:12:29 +0530

    Creating a nukechannel command
    This command if used deletes the whole channel in which it's used and then recreates the same channel with the same user permissions. The user needs to have the 'Manage Channels' permission to use this command.

---
+++ commands/admin/nukechannel.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-useless-escape */
+const Discord = require("discord.js");
+
+
+/***
+* @param {Discord.client} bot the discord bot client.
+* @param {Discord.messsage} message the initial message sent by the user.
+* @param {array} args an array of arguments
+ */
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You cannot use this command");
+    if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("My doesn't have the permission \`MANAGE_CHANNELS\`");
+    
+    let reason = args.join(" ");
+    const nukeChannel = message.channel;
+
+    if (!reason) reason = "No reason given.";
+
+    await nukeChannel.clone().catch(err => console.log(err));
+    await nukeChannel.delete(reason).catch(err => console.log(err));
+
+
+};
+
+/***
+ * Exports the nukechannel command to the help object
+ */
+
+module.exports.help = {
+    name: "nukechannel",
+    aliases: ['nc']
+};
