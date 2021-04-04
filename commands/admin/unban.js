df4f18ca1099de8f19d5f45766bffda5ea57b880
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2021-04-04 15:58:18 +0530

    Made a couple of changes

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -6,29 +6,30 @@ const discord = require("discord.js");
  */
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You donot have the permissison to ban someone.");
-    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('My role does not have \`BAN_MESSAGES\` permission.');
-    let reason = args.slice(1).join(" ");
-    let userID = args[0]
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('My role does not have `BAN_MEMBERS` permission.');
+    let reason = args.splice(1).join(" ");
+    let userID = args[0];
     if (!reason) reason = 'No reason given.';
-    if (!args[0]) return message.channel.send('You must someone to unban. \`.unban ID reason\`');
-    if (isNaN(args[0])) return message.channel.send('The ID stated is not a number. \`.unban ID reason\`');
+    if (!args[0]) return message.channel.send('You must someone to unban.');
+    if (isNaN(args[0])) return message.channel.send('The ID stated is not a number.');
     message.guild.fetchBans().then(async bans => {
-      if (bans.size == 0) return message.channel.send('This server does not have anyone banned');
-      let bUser = bans.find(b => b.user.id == userID);
-      if (!bUser) return message.channel.send('The user ID stated is not banned.');
-      await message.guild.members.unban(bUser.user, reason).catch(err => {
-        console.log(err);
-        return message.channel.send('Something went wrong unbanning the id.');
-      }).then(() => {
-        message.channel.send(`Succesfully Unbanned ${args[0]}`);
-      });
+        if (bans.size == 0) return message.channel.send('This server does not have anyone banned');
+        let bUser = bans.find(b => b.user.id == userID);
+        if (!bUser) return message.channel.send('The user ID stated is not banned.');
+        await message.guild.members.unban(bUser.user, reason).catch(err => {
+            console.log(err);
+            return message.channel.send('Something went wrong unbanning the id.');
+        }).then(() => {
+            message.channel.send(`Succesfully Unbanned ${args[0]}`);
+        });
 
+    });
 };
 
 /***
- * Exports the ban command to the help object
+ * Exports the unban command to the help object
  */
 module.exports.help = {
-	name: "unban",
-	aliases: []
+    name: "unban",
+    aliases: []
 };
